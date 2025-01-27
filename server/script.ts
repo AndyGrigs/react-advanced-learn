46c3835532669ee23e8e9af4f465a8a263d1f843
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2025-01-27 22:48:43 +0100

    added and tested local server

--- server/script.ts
+++ server/script.ts
@@ -1,25 +1,65 @@
-/* eslint-disable linebreak-style */
+import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
 
+const app = express();
 const prisma = new PrismaClient();
 
-async function main() {
-    // ... you will write your Prisma Client queries here
-    const user = await prisma.user.create({
-        data: {
-            name: 'Alice',
-            email: 'alice@prisma.io',
-        },
-    });
-    console.log(user);
-}
+app.use(express.json());
 
-main()
-    .then(async () => {
-        await prisma.$disconnect();
-    })
-    .catch(async (e) => {
-        console.error(e);
-        await prisma.$disconnect();
-        process.exit(1);
-    });
+app.get('/users', async (req, res) => {
+    try {
+        const users = await prisma.user.findMany();
+        res.json(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// eslint-disable-next-line consistent-return
+app.post('/user', async (req, res) => {
+    try {
+        console.log(req.body);
+        const { name, email, password } = req.body;
+        if (!password) {
+            return res.status(400).send('No password provided!');
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword,
+            },
+        });
+
+        // Don’t return the password (even if hashed):
+        const { password: _, ...result } = user;
+        res.json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// app.post('/user', async (req, res) => {
+//     try {
+//         const { name, email } = req.body;
+//         const user = await prisma.user.create({
+//             data: { name, email },
+//         });
+//         res.json(user);
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send('Internal Server Error');
+//     }
+// });
+
+const PORT = process.env.PORT || 4445;
+
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+    await prisma.$disconnect();
+});
