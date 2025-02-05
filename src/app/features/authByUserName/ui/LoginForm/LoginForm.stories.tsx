5a7db7b8f8aa65ddae7e782443a02e4cb3c3d980
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2025-02-05 21:23:43 +0100

    async modal added

--- src/app/features/authByUserName/ui/LoginForm/LoginForm.stories.tsx
+++ src/app/features/authByUserName/ui/LoginForm/LoginForm.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Meta, StoryFn } from '@storybook/react';
 import { StoreProvider } from 'app/providers/StoreProvider';
-import { LoginForm } from './LoginForm';
+import LoginForm from './LoginForm';
 
 export default {
     title: 'features/LoginForm', // Fixed path structure
--- src/app/features/authByUserName/ui/LoginForm/LoginForm.tsx
+++ src/app/features/authByUserName/ui/LoginForm/LoginForm.tsx
@@ -15,7 +15,7 @@ export interface LoginFormProps {
   className?: string;
 }
 
-export const LoginForm = memo(({ className }: LoginFormProps) => {
+const LoginForm = memo(({ className }: LoginFormProps) => {
     const { t } = useTranslation();
     const dispatch = useDispatch();
     const {
@@ -64,3 +64,5 @@ export const LoginForm = memo(({ className }: LoginFormProps) => {
         </div>
     );
 });
+
+export default LoginForm;
