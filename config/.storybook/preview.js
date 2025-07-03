596972c5d9c0e0b696650bcd16ce373536ab871c
Andrii Grygorov <97991761+AndyGrigs@users.noreply.github.com>
2025-07-03 11:34:41 +0200

    Update preview.js

--- config/.storybook/preview.js
+++ config/.storybook/preview.js
@@ -3,25 +3,33 @@ import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
 import { StoreDecorator } from '../../src/shared/config/storybook/StoreDecorator/StoreDecorator';
-import 'regenerator-runtime/runtime'; 
+import 'regenerator-runtime/runtime';
 
-
-export const parameters = {
-    actions: { argTypesRegex: '^on[A-Z].*' },
-    controls: {
-        matchers: {
-            color: /(background|color)$/i,
-            date: /Date$/,
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+    parameters: {
+        actions: { argTypesRegex: '^on[A-Z].*' },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/,
+            },
         },
+        layout: 'fullscreen',
     },
+    
+    decorators: [
+        StyleDecorator,
+        ThemeDecorator(Theme.LIGHT),
+        RouterDecorator,
+        (Story) => (
+            <StoreDecorator initialState={{}}>
+                <Story />
+            </StoreDecorator>
+        ),
+    ],
+
+    tags: ['autodocs'],
 };
 
-export const decorators = [
-    // Order matters - last decorator wraps everything
-    (Story) => StyleDecorator(Story),
-    (Story) => ThemeDecorator(Theme.LIGHT)(Story),
-    (Story) => RouterDecorator(Story),
-    (Story) => StoreDecorator(Story),
-];
-
-export const tags = ['autodocs'];
+export default preview;
