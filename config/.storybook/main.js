c03be3992b59a52a9b00fb7a22723c8e1f6fe226
Andrii Grygorov <97991761+AndyGrigs@users.noreply.github.com>
2025-07-03 11:22:26 +0200

    Update main.js

--- config/.storybook/main.js
+++ config/.storybook/main.js
@@ -13,16 +13,43 @@ module.exports = {
 
     framework: {
         name: "@storybook/react-webpack5",
-        options: {}
+        options: {
+            builder: {
+                useSWC: true,
+            }
+        }
     },
 
-    testRunner: {
-        preset: 'playwright'
-      },
-
-    docs: {},
-
     typescript: {
-        reactDocgen: 'react-docgen-typescript'
+        reactDocgen: 'react-docgen-typescript',
+        reactDocgenTypescriptOptions: {
+            shouldExtractLiteralValuesFromEnum: true,
+            propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+        },
+    },
+
+    webpackFinal: async (config) => {
+        // Додаємо підтримку абсолютних імпортів
+        config.resolve.modules = [
+            ...(config.resolve.modules || []),
+            path.resolve(__dirname, '..', '..', 'src'),
+        ];
+
+        // Додаємо розширення файлів
+        config.resolve.extensions = [
+            ...(config.resolve.extensions || []),
+            '.ts',
+            '.tsx'
+        ];
+
+        return config;
+    },
+
+    docs: {
+        autodocs: 'tag',
+    },
+
+    features: {
+        storyStoreV7: true,
     }
 };
